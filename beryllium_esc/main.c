4dcc206dd2407a604c349015c76700252f89a699
Scott Dixon <scottd.nerd@gmail.com>
2017-04-07 22:03:39 -0700

    Adding JLink loadfile script.

--- beryllium_esc/main.c
+++ beryllium_esc/main.c
@@ -57,17 +57,17 @@ SysTick_Handler(void)
     port_pin_toggle_output_level(LED_0_PIN);
 }
 
-///** Configure LED0, turn it off*/
-// static void
-// config_led(void)
-//{
-//    struct port_config pin_conf;
-//    port_get_config_defaults(&pin_conf);
-//
-//    pin_conf.direction = PORT_PIN_DIR_OUTPUT;
-//    port_pin_set_config(LED_0_PIN, &pin_conf);
-//    port_pin_set_output_level(LED_0_PIN, LED_0_INACTIVE);
-//}
+/** Configure LED0, turn it off*/
+static void
+config_led(void)
+{
+    struct port_config pin_conf;
+    port_get_config_defaults(&pin_conf);
+
+    pin_conf.direction = PORT_PIN_DIR_OUTPUT;
+    port_pin_set_config(LED_0_PIN, &pin_conf);
+    port_pin_set_output_level(LED_0_PIN, LED_0_INACTIVE);
+}
 
 #define PERIPHERAL_ADDR 0x12
 
@@ -97,9 +97,9 @@ main(void)
     system_init();
 
     /*Configure system tick to generate periodic interrupts */
-    // SysTick_Config(system_gclk_gen_get_hz(GCLK_GENERATOR_0));
+    SysTick_Config(system_gclk_gen_get_hz(GCLK_GENERATOR_0));
 
-    // config_led();
+    config_led();
 
     configure_i2c_master();
 
